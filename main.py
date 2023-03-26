08ff4c3401e4bef386df71fab601f8908083bcd0
fathimasilviyak <silviyakvm@gmail.com>
2023-03-26 15:10:33 +0530

    Create a turtle player that starts at the bottom of the screen, and move it north for the Up keypress.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import time
 from turtle import Screen
+from player import Player
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("TURTLE CROSSING GAME")
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.move, "Up")
+
 
 game_is_on = True
 while game_is_on:
