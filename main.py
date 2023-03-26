43d08e9bc72c30e9e79bb3336bf4c0614a06b3ad
fathimasilviyak <silviyakvm@gmail.com>
2023-03-26 14:54:10 +0530

    Create screen

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+from turtle import Screen
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("TURTLE CROSSING GAME")
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
